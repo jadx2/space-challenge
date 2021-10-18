a3d5277a018f1dfe1ad28f998ee0726fc4fd1763
jadx2 <delmarjaim@gmail.com>
2021-10-18 18:18:58 -0500

    Create loadU1 method to load as many U1 rockets as possible

--- src/simulation.kt
+++ src/simulation.kt
@@ -1,27 +1,34 @@
+import design.Item
+import design.U1
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.URL
 
 fun main() {
     val newSimulation = Simulation()
-    print(newSimulation.loadItems("https://s3.amazonaws.com/video.udacity-data.com/topher/2017/December/5a372d67_phase-1/phase-1.txt"))
+    val items = newSimulation.loadItems("https://s3.amazonaws.com/video.udacity-data.com/topher/2017/December/5a372d67_phase-1/phase-1.txt")
+    newSimulation.loadU1(items)
 }
 
 class Simulation {
-    fun loadItems(url: String): MutableList<Map<String, Int>> {
-        val items = mutableListOf<Map<String, Int>>()
+    fun loadItems(url: String): MutableList<Item> {
+        val items = mutableListOf<Item>()
         val text = BufferedReader(InputStreamReader(URL(url).openStream()))
         val linesInText: MutableList<List<String>> = mutableListOf()
 
         text.forEachLine { linesInText.add(it.split("=")) }
 
         for (line in linesInText) {
-            val itemObject = mapOf<String, Int>(line[0] to line[1].toInt())
-            items.add(itemObject)
+            val item = Item(line[0], line[1].toInt())
+            items.add(item)
         }
 
         return items
     }
 
-
+    fun loadU1(items: MutableList<Item>) : MutableList<U1> {
+        val u1Fleet = mutableListOf<U1>()
+        print(items)
+        return u1Fleet
+    }
 }
