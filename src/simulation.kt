7a2009bd64c8b77b3bda576ad90626899219acbf
jadx2 <delmarjaim@gmail.com>
2021-10-18 15:23:16 -0500

    Create loadItems in Simulation class to create list key-pair collection from the items in the list given

--- src/simulation.kt
+++ src/simulation.kt
@@ -1,11 +1,25 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.URL
 
-fun main(args: Array<String>){
+fun main() {
     val newSimulation = Simulation()
-    newSimulation.loadItems("https://s3.amazonaws.com/video.udacity-data.com/topher/2017/December/5a372d67_phase-1/phase-1.txt")
+    print(newSimulation.loadItems("https://s3.amazonaws.com/video.udacity-data.com/topher/2017/December/5a372d67_phase-1/phase-1.txt"))
 }
-class Simulation() {
-    fun loadItems(url: String) {
-        val data = URL(url).readText(Charsets.UTF_8)
+
+class Simulation {
+    fun loadItems(url: String): MutableList<Map<String, Int>> {
+        val items = mutableListOf<Map<String, Int>>()
+        val text = BufferedReader(InputStreamReader(URL(url).openStream()))
+        val linesInText: MutableList<List<String>> = mutableListOf()
+
+        text.forEachLine { linesInText.add(it.split("=")) }
+
+        for (line in linesInText) {
+            val itemObject = mapOf<String, Int>(line[0] to line[1].toInt())
+            items.add(itemObject)
+        }
+
+        return items
     }
 }
